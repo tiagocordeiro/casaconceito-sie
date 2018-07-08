0554c5671f3786374f3ab8065c330b21253cbeed
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-08 03:42:30 -0300

    Corrige path static jet

--- casaconceito/urls.py
+++ casaconceito/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
+
+# from django.contrib import admin
+# from django.urls import path, re_path, include
 
 from pages.views import home_view
+from casaconceito import settings
 
 urlpatterns = [
-    re_path(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    re_path(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    # re_path(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    # re_path(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
