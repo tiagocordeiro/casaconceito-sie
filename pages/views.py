3ae3a436a22364290806dbf9c067fa5bcdfaaad5
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-09 08:49:28 -0300

    Configura login_require para view home_view

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
+@login_required
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Olá</h1>")
