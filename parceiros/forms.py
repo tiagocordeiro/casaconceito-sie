8f9fa5540aaa095934c1acbbe891a0cb5e194c2e
Tiago Cordeiro <tiago@mulhergorila.com>
2018-07-13 17:53:54 -0300

    Testes com form fields para telefone e celular

--- parceiros/forms.py
+++ parceiros/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Indicacao
 
 
@@ -7,6 +7,11 @@ class IndicacaoForm(ModelForm):
         model = Indicacao
         fields = ['cliente', 'descricao', 'email', 'telefone',
                   'celular', ]
+
+        widgets = {
+            'telefone': TextInput(attrs={'class': 'phone', 'type': 'tel'}),
+            'celular': TextInput(attrs={'class': 'phone', 'type': 'tel'})
+        }
 
 
 class IndicacaoEditForm(ModelForm):
